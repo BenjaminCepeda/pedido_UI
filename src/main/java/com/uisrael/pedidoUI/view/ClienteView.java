fe767f9204cdc404758760e35497c421b2073933
Benjamin Cepeda <bcepeda_98@yahoo.com>
2022-01-19 22:59:48 -0500

    Avance Componentes Primefaces

--- src/main/java/com/uisrael/pedidoUI/view/ClienteView.java
+++ src/main/java/com/uisrael/pedidoUI/view/ClienteView.java
@@ -1,10 +1,16 @@
 package com.uisrael.pedidoUI.view;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.PrimeFaces;
 
 import com.uisrael.pedidoUI.controller.ClienteController;
 import com.uisrael.pedidoUI.controller.impl.ClienteControllerImpl;
@@ -21,17 +27,26 @@ public class ClienteView implements Serializable{
 
 	private ClienteController clienteController;
 	private Cliente cliente;
+	private List<Cliente> listaClientes = new ArrayList<Cliente>();
 
 	@PostConstruct
 	public void init() {
 		clienteController = new ClienteControllerImpl();
-		cliente = new Cliente();		
-		
+		cliente = new Cliente();	
+		listarClientes();
 	}
 	
 	public void insertar() {
 		clienteController.insertar(cliente);
+		listarClientes();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Registro Ingresado"));
+		PrimeFaces.current().ajax().update("frmCliente:mensaje", "frmCliente:dtClientes");	
 	}
+	
+	public void listarClientes() {
+		listaClientes = clienteController.listar();
+	}
+	
 	
 	public Cliente getCliente() {
 		return cliente;
@@ -40,5 +55,14 @@ public class ClienteView implements Serializable{
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
+	public List<Cliente> getListaClientes() {
+		return listaClientes;
+	}
+
+	public void setListaClientes(List<Cliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+	
 	
 }
